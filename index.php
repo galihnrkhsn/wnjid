f8c212b4f5e58408f59f80c08b3713f5e8e1743b
Galih <galihnurikhsan24@gmail.com>
2026-07-22 15:55:33 +0700

    update

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
    ini_set('display_errors', '1');
    ini_set('display_startup_errors', '1');
    session_start();
-   include '../includes/db.php';
+   include 'includes/db.php';
 
    $loginError         = '';
    $loginErrorRedirect = '';
