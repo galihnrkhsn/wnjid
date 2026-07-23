26f4c210192956b08f94ba804a91b76bcc9aec78
Galih <galihnurikhsan24@gmail.com>
2026-07-23 11:55:27 +0700

    update

--- distributor/formpembayarana.php
+++ distributor/formpembayarana.php
@@ -274,6 +274,38 @@
 <!-- PEMBAYARAN BANK -->
 <?php
     include "koneksi.php";
+
+    if (!function_exists('convertUploadedImageToWebp')) {
+        function convertUploadedImageToWebp(string $tmpPath, string $extension, string $destDir, string $baseName, int $quality = 85) {
+            switch ($extension) {
+                case 'jpg':
+                case 'jpeg':
+                    $image = @imagecreatefromjpeg($tmpPath);
+                    break;
+                case 'png':
+                    $image = @imagecreatefrompng($tmpPath);
+                    break;
+                default:
+                    $image = false;
+            }
+
+            if (!$image) {
+                return null;
+            }
+
+            // Jaga transparansi kalau sumbernya PNG
+            imagepalettetotruecolor($image);
+            imagealphablending($image, true);
+            imagesavealpha($image, true);
+
+            $filename = $baseName . '.webp';
+            $saved    = imagewebp($image, $destDir . $filename, $quality);
+            imagedestroy($image);
+
+            return $saved ? $filename : null;
+        }
+    }
+
     if(isset($_POST['kirimBank'])){
 
         date_default_timezone_set('Asia/Jakarta');
@@ -289,7 +321,7 @@
         $foto                   = $_FILES['foto']['name'];
         $tmp                    = $_FILES['foto']['tmp_name'];
         $ukuranFile             = $_FILES['foto']['size'];
-        $ekstensiGambarValid    = ['jpg','jpeg','png','svg'];
+        $ekstensiGambarValid    = ['jpg','jpeg','png'];
         $ekstensiGambar         = explode('.', $foto);
         $ekstensiGambar         = strtolower(pathinfo($foto, PATHINFO_EXTENSION));
         $errors                 = [];
@@ -305,11 +337,9 @@
         }
 
         if (empty($errors)) {
-            $namaFileBaru  = 'DB' . uniqid();
-            $namaFileBaru .= '.';
-            $namaFileBaru .= $ekstensiGambar;
+            $namaFileBaru = convertUploadedImageToWebp($tmp, $ekstensiGambar, '../image/bukti_transfer/', 'DB' . uniqid());
 
-            if (move_uploaded_file($tmp, '../image/bukti_transfer/' . $namaFileBaru)) {
+            if ($namaFileBaru) {
                 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
                 $koneksi->begin_transaction();
                 try {
@@ -396,7 +426,7 @@
         $foto       = $_FILES['foto']['name'];
         $tmp        = $_FILES['foto']['tmp_name'];
         $ukuranFile = $_FILES['foto']['size'];
-        $ekstensiGambarValid = ['jpg', 'jpeg', 'png', 'svg'];
+        $ekstensiGambarValid = ['jpg', 'jpeg', 'png'];
         $ekstensiGambar      = explode('.', $foto);
         $ekstensiGambar      = strtolower(pathinfo($foto, PATHINFO_EXTENSION));
         $errors              = [];
@@ -411,11 +441,9 @@
         }
 
         if (empty($errors)) {
-            $namaFileBaru = uniqid();
-            $namaFileBaru.='.';
-            $namaFileBaru.= $ekstensiGambar;
+            $namaFileBaru = convertUploadedImageToWebp($tmp, $ekstensiGambar, '../image/bukti_transfer/', uniqid());
 
-            if (move_uploaded_file($tmp, '../image/bukti_transfer/' . $namaFileBaru)) {
+            if ($namaFileBaru) {
                 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
                 $koneksi->begin_transaction();
                 try {
--- distributor/transaksi.php
+++ distributor/transaksi.php
@@ -1,6 +1,4 @@
 <?php 
-	session_start();
-
 	include 'koneksi.php'; 
 	include 'assets/components/Sessions/sesDistri.php';
 ?>
